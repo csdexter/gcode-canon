55ae9af87754d464ba6f7f843b3e6f109d68d717
Radu - Eosif Mihailescu <radu.mihailescu@linux360.ro>
2013-08-17 20:24:30 +0100

    UPDATE: - sane canned cycles :-) - one batch of drilling/boring cycles are in, a couple more remaining.

--- gcode-cycles.c
+++ gcode-cycles.c
@@ -21,12 +21,12 @@
 #include "gcode-math.h"
 #include "gcode-debugcon.h"
 #include "gcode-machine.h"
+#include "gcode-parameters.h"
 
 
-static const char GCODE_CYCLE_PRE_1[] = "G00 X%4.2f Y%4.2f\n";
-static const char GCODE_CYCLE_PRE_2[] = "G00 Z%4.2f\n";
+static const char GCODE_CYCLE_PRE_1[] = "G00 Z%4.2f\n";
+static const char GCODE_CYCLE_PRE_2[] = "G00 X%4.2f Y%4.2f\n";
 static const char GCODE_CYCLE_POST[] = "G53 G%02d Z%4.2f\n";
-static const char GCODE_DRILL_NDWELL[] = "G01 Z%4.2f\n";
 
 static uint8_t curSlice;
 static char *slices[GCODE_CYCLE_MAXSLICES];
@@ -39,13 +39,14 @@ static bool _add_generated_line(const char *format, ...) {
   vsnprintf(slices[0], GCODE_CYCLE_BUFSLICE, format, ap);
   va_end(ap);
 
-  if((strlen(slices[curSlice]) + strlen(slices[0])) >= GCODE_CYCLE_BUFSLICE)
+  if((strlen(slices[curSlice]) + strlen(slices[0])) >= GCODE_CYCLE_BUFSLICE) {
     if(curSlice < GCODE_CYCLE_MAXSLICES)
       slices[curSlice++] = (char *)calloc(GCODE_CYCLE_BUFSLICE, 1);
     else {
       display_machine_message("PER: Canned cycle injection buffer overflow!");
       return false;
     }
+  }
 
   strncat(slices[curSlice], slices[0], GCODE_CYCLE_BUFSLICE);
   slices[0][0] = '\0';
@@ -68,7 +69,7 @@ static char *_build_cycle(void) {
   char *cycle;
 
   for(i = curSlice; i; i--) size += strlen(slices[i]);
-  cycle = (char *)calloc(size, 1);
+  cycle = (char *)calloc(size + 1, 1);
   for(i = 1; i <= curSlice; i++) strncat(cycle, slices[i], size);
 
   return cycle;
@@ -86,30 +87,80 @@ bool init_cycles(void *data) {
 }
 
 char *generate_cycles(TGCodeState state) {
-  double preZ = state.system.Z;
+  double preZ = state.system.Z, pregZ = state.system.gZ, precZ;
   bool feedRetract;
+  uint16_t rep;
 
-  /* First preparatory move */
-  _add_generated_line(GCODE_CYCLE_PRE_1, state.system.cX, state.system.cY);
-
-  /* Second preparatory move */
+  /* Preparatory move (singleton, only if Z below R) */
   do_WCS_move_math(&state.system, NAN, NAN, state.R);
-  if(state.system.Z > preZ) _add_generated_line(GCODE_CYCLE_PRE_2, state.R);
+  if(state.system.Z > preZ) {
+    _add_generated_line(GCODE_CYCLE_PRE_1, state.R);
+  }
   state.system.Z = preZ;
+  state.system.gZ = pregZ;
 
-  /* Actual canned cycle */
-  //TODO: handle repeats (in both absolute and relative mode)
-  switch(state.cycle) {
-    case GCODE_CYCLE_DRILL_ND:
-      _add_generated_line(GCODE_DRILL_NDWELL, state.system.cZ);
-      feedRetract = false;
-      break;
+  if(state.system.absolute == GCODE_RELATIVE) {
+    //Compute the previous Z relative to R in case we're in G98
+    precZ = 0 - state.R;
+
+    //Z is relative to current position, R was relative to the current
+    //position and needs to be made relative to Z
+    state.R = 0 - state.system.cZ;
+  } else
+    //Reverse engineer the previous Z in case we're in G98
+    precZ = state.system.gZ - (
+        state.system.current == GCODE_MCS ?
+            0.0 :
+            fetch_parameter(
+                GCODE_PARM_FIRST_WCS +
+                (state.system.current - GCODE_WCS_1) *
+                GCODE_PARM_WCS_SIZE + GCODE_AXIS_Z) + state.system.offset.Z);
+
+  /* Repetitions */
+  for(rep = 0; rep < state.L; rep++) {
+    /* First preparatory move */
+    _add_generated_line(GCODE_CYCLE_PRE_2, state.system.cX, state.system.cY);
+    /* Second preparatory move */
+    _add_generated_line(GCODE_CYCLE_PRE_1, state.R);
+    /* Actual canned cycle */
+    switch(state.cycle) {
+      case GCODE_CYCLE_DRILL_ND:
+      case GCODE_CYCLE_DRILL_WD:
+      case GCODE_CYCLE_BORING_ND_NS:
+      case GCODE_CYCLE_BORING_WD_WS:
+      case GCODE_CYCLE_BORING_MANUAL:
+      case GCODE_CYCLE_BORING_WD_NS:
+        _add_generated_line("G01 Z%4.2f\n", state.system.cZ);
+        if(state.cycle == GCODE_CYCLE_DRILL_WD ||
+           state.cycle == GCODE_CYCLE_BORING_WD_WS ||
+           state.cycle == GCODE_CYCLE_BORING_MANUAL ||
+           state.cycle == GCODE_CYCLE_BORING_WD_NS)
+          _add_generated_line("G04 P%4.2f\n", state.P);
+        if(state.cycle == GCODE_CYCLE_BORING_WD_WS ||
+           state.cycle == GCODE_CYCLE_BORING_MANUAL)
+          _add_generated_line("M05\n");
+        if(state.cycle == GCODE_CYCLE_BORING_MANUAL)
+          _add_generated_line("M01\n");
+        if(state.cycle == GCODE_CYCLE_BORING_ND_NS ||
+           state.cycle == GCODE_CYCLE_BORING_WD_NS)
+          feedRetract = true;
+        else
+          feedRetract = false;
+        break;
+    }
+    /* Final move */
+    _add_generated_line(GCODE_CYCLE_POST, (int)feedRetract, state.R);
   }
 
-  /* Final move */
-  _add_generated_line(
-      GCODE_CYCLE_POST, (int)feedRetract,
-      (state.retractMode == GCODE_RETRACT_LAST ? preZ : state.R));
+  /* Retract to last Z if in G98 */
+  if(state.retractMode == GCODE_RETRACT_LAST)
+    _add_generated_line(GCODE_CYCLE_POST, (int)false, precZ);
+
+  /* Any extras ? */
+  if(state.cycle == GCODE_CYCLE_BORING_WD_WS ||
+     state.cycle == GCODE_CYCLE_BORING_MANUAL)
+    //TODO: make it start in the same direction it was turning before
+    _add_generated_line("M03\n");
 
   return _build_cycle();
 }
